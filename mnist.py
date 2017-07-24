92ede2d5e456857b6904b239350f86fffda5edd4
develask <develascomikel@gmail.com>
2017-07-24 10:01:56 +0200

    mnist

---
+++ mnist.py
@@ -0,0 +1,123 @@
+import dnn
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import time
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+nn = dnn.DNN("mnist")
+
+sections = []
+for i in range(16):
+	nn_ = dnn.DNN("Compute section_"+str(i))
+	raw = dnn.Input(7, "raw_s_"+str(i+1))
+	h1 = dnn.Fully_Connected(7, "sigmoid", "r1_"+str(i+1))
+	h2 = dnn.Fully_Connected(7, "sigmoid", "r2_"+str(i+1))
+
+	raw.addNext(h1)
+	h1.addNext(h2)
+
+	h1.addNextRecurrent(h1)
+	h2.addNextRecurrent(h2)
+
+	nn_.add_inputs(raw)
+
+	sections.append(nn_)
+
+
+raws = [dnn.Input(7, "raw "+str(i+1)) for i in range(16)]
+
+# h1s = [dnn.Fully_Connected(28, "sigmoid", "h1_"+str(i+1)) for i in range(16)]
+# h2s = [dnn.Fully_Connected(16, "sigmoid", "h2_"+str(i+1)) for i in range(16)]
+
+h3 = dnn.Fully_Connected(64, "sigmoid", "h3")
+
+ou = dnn.Fully_Connected(10, "softmax", "output")
+
+for i in range(16):
+	raws[i].addNext(sections[i])
+	sections[i].addNext(h3)
+
+# for i in range(16):
+# 	raws[i].addNext(h1s[i])
+# 	h1s[i].addNext(h2s[i])
+# 	h2s[i].addNext(h3)
+h3.addNext(ou)
+
+for i in range(16):
+	nn.add_inputs(raws[i])
+
+nn.initialize()
+
+
+print("data transposing")
+
+xs, ys = mnist.train.next_batch(mnist.train.num_examples)
+training_data_tmp = list(zip(xs,ys))
+training_data = []
+
+def convert_img(img):
+	im = img.reshape((28,28))
+	inputs = []
+	for i in range(4):
+		x_min = i*7
+		x_max = x_min+7
+		for j in range(4):
+			y_min = j*7
+			y_max = y_min + 7
+			inputs.append(im[x_min:x_max,y_min:y_max])
+	inp = []
+	for i in range(inputs[0].shape[0]):
+		tmp = []
+		for box in inputs:
+			tmp.append(box[i,:,np.newaxis])
+		inp.append(tmp)
+	return inp
+	#return [ img[:,np.newaxis] ]
+
+
+for example in training_data_tmp[:200]:
+	training_data.append((convert_img(example[0]), example[1][:,np.newaxis]))
+
+len_data = len(training_data)
+
+print("data training")
+start = time.clock()
+nn.SGD(training_data[int(len_data*0.01):], training_data[:int(len_data*0.01)], 128, 4, 0.1, 0.05)
+end = time.clock()
+interval_c = end-start
+print("Time training: %.2gs" % interval_c)
+
+#models.save(nn)
+import pickle as pickle
+pickle.dump( nn, open( "save.p", "wb" ) )
+print("saved")
+
+print("data testing")
+xs, ys = mnist.test.next_batch(mnist.test.num_examples)
+good = 0
+total = 0
+rmat = np.zeros((10,10))
+fallados = []
+for i in range(mnist.test.num_examples):
+	em = nn.prop(convert_img(xs[i]))
+	pred = np.argmax(em)
+	real = np.argmax(ys[i])
+	rmat[pred,real] += 1
+	if pred == real:
+		good+=1
+	else:
+		fallados.append([xs[i].reshape((28,28)), pred, real])
+	total+=1
+print("rate:", good/total, "(",good,"/",total,")")
+print("-----------------------------------------------")
+print("row is real")
+print("col is predicted")
+print(rmat.astype(int))
+q = input("> quieres ver las fotos que falla ("+str(len(fallados))+") (s/n)?  ")
+if q == "s":
+	import matplotlib.pyplot as plt
+	for fallo in fallados:
+		plt.title("pred: "+str(fallo[1])+' - real: '+str(fallo[2]))
+		plt.imshow(fallo[0])
+		plt.show()
